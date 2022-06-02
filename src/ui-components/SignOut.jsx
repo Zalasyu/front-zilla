14a421f129fc2c1a3ff7037df5bd7c3c504e66bc
Alec Moldovan <alec_moldovan@yahoo.com>
2022-06-02 17:05:49 -0500

    feat: Sign out button works.

--- src/ui-components/SignOut.jsx
+++ src/ui-components/SignOut.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useMsal } from "@azure/msal-react";
+import { Button } from "@mui/material";
 
 function handleLogout(instance) {
     instance.logoutRedirect().catch( e => {
