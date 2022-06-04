a99c1820bfda37fc2d5a7f21037a62e6dc794861
Alec Moldovan <alec_moldovan@yahoo.com>
2022-06-03 20:26:00 -0500

    Added a tooltip message to signoutsignin button.

--- src/ui-components/SignInOrSignOut/SignInSignOutButton.jsx
+++ src/ui-components/SignInOrSignOut/SignInSignOutButton.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+// React Imports
+import React, { useState } from "react";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
+import { IconButton, Tooltip } from "@mui/material";
+
+// MSAL Imports
 import { InteractionStatus } from "@azure/msal-browser";
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+
+
 
 const SignInSignOutButton = () => {
     const { inProgress } = useMsal();
     const isAuthenticated = useIsAuthenticated();
 
+
     if (isAuthenticated) {
-        return < SignOutButton />;
+        return ( 
+        <Tooltip title='Click to Sign Out' arrow>
+            <IconButton>
+                <SignOutButton />
+            </IconButton>
+        </Tooltip>
+        );
     } else if (inProgress !== InteractionStatus.Startup && inProgress !== InteractionStatus.HandleRedirect) {
-        return <SignInButton />;
+        return( 
+        
+        <Tooltip title='Click to Sign Out' arrow>
+            <IconButton>
+                <SignInButton />
+            </IconButton>
+        </Tooltip>
+        
+        
+        );
     } else {
         return null;
     }
